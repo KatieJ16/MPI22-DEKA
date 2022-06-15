d16426caef3c29068d6d38c7600583fed6a44f60
Katie Johnston <katie@acornonsite.com>
2022-06-15 09:51:40 -0400

    Update simple_sim_wpi2.py

--- simple_sim_wpi2.py
+++ simple_sim_wpi2.py
@@ -8,17 +8,20 @@ Created on Mon Jun 13 16:26:21 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-lam_list = [0.2]#np.arange(0,1.1, .1) #the list of lambdas to test
+lam_list = np.arange(0,1.1, .1) #the list of lambdas to test
 to_plot = True
 
+vmax = 4.0
+amax = 2.0
 
-for num in range(1): #we are repeating everything 10 times
+
+for num in range(10): #we are repeating everything 10 times
     #get initial start spot
     step_n_list = list()
-    x = np.random.uniform(0.5,1, 2)
+    x = np.random.uniform(0.5,1, 2) #starting point is outside our circle
     v = np.random.uniform(0,1, 2)
-    if np.linalg.norm(v) > 1.0:
-        v = v/np.linalg.norm(v)
+    if np.linalg.norm(v) > vmax:
+        v = v/np.linalg.norm(v) * vmax
     x0 = x
     v0 = v
      
@@ -26,26 +29,28 @@ for num in range(1): #we are repeating everything 10 times
     #run for every lambda 
     for lam in lam_list:
         
-        #set beginning values
+        #set beginning values, so it's the same everytime
         x = x0
         v = v0
         
         #plot the start
-        # print("v = ", v)
         if to_plot:
             plt.plot(x[0], x[1], '*')
             plt.plot(0,0,'k*')
             plt.arrow(x[0], x[1], v[0], v[1])
         
-        a = -x / np.linalg.norm(x)
+        #get starting a
+        a = ((1-lam)*v+lam*x)
+        a = -a/np.linalg.norm(a) * amax
         
-        # x_list = list()
-        # v_list = list()
-        # a_list = list()
-        
-        # x_list.append(np.linalg.norm(x))
-        # v_list.append(np.linalg.norm(v))
-        # a_list.append(np.linalg.norm(a))
+        if to_plot:
+            x_list = list()
+            v_list = list()
+            a_list = list()
+            
+            x_list.append(np.linalg.norm(x))
+            v_list.append(np.linalg.norm(v))
+            a_list.append(np.linalg.norm(a))
         
 
         #setting some parameters
@@ -64,17 +69,19 @@ for num in range(1): #we are repeating everything 10 times
             
             #take a step in space
             x = x + v * dt
-            # x_list.append(np.linalg.norm(x))
-            # v_list.append(np.linalg.norm(v))
-            # a_list.append(np.linalg.norm(a))
+            
+            if to_plot:
+                x_list.append(np.linalg.norm(x))
+                v_list.append(np.linalg.norm(v))
+                a_list.append(np.linalg.norm(a))
             
             #get the direction of a
             a = ((1-lam)*v+lam*x)
             
             #get magnitude of a
-            a = -a/np.linalg.norm(a) #outside circle
+            a = -a/np.linalg.norm(a) * amax#outside circle
             if np.linalg.norm(x) < 1/2: #hit the breaks, inside circle
-                a = a/np.linalg.norm(a)
+                a = a/np.linalg.norm(a) * amax
             if to_plot:
                 plt.plot(x[0], x[1], '.')
             
@@ -104,11 +111,16 @@ for num in range(1): #we are repeating everything 10 times
             
             plt.plot( x, y, 'k' ) 
             
+            plt.title("Path with lambda = " +str(lam))
+            
             plt.show()
-        # plt.semilogy(x_list, label = "x")
-        # plt.plot(v_list, label = "v")
-        # plt.plot(a_list, label = "a")
-        # plt.legend()
+            plt.semilogy(x_list, label = "x")
+            plt.plot(v_list, label = "v")
+            plt.plot(a_list, label = "a")
+            plt.legend()
+            plt.show()
     
     plt.semilogy(lam_list, step_n_list)
-    
+plt.xlabel("lambda")
+plt.ylabel("timesteps to origin (Max at 10^4)")
+plt.title("Timesteps to origin for 10 different initial conditions")    
