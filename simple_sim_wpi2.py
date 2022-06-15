18ecb29fffebfbaa40e70fc8c1098d0028ed89d2
Katie Johnston <katie@acornonsite.com>
2022-06-15 15:21:46 -0400

    Update simple_sim_wpi2.py

--- simple_sim_wpi2.py
+++ simple_sim_wpi2.py
@@ -8,18 +8,18 @@ Created on Mon Jun 13 16:26:21 2022
 import numpy as np
 import matplotlib.pyplot as plt
 
-lam_list = np.arange(0,1.1, .1) #the list of lambdas to test
+lam_list = [0.7]#np.arange(0,1.1, .1) #the list of lambdas to test
 to_plot = True
 
 vmax = 4.0
 amax = 2.0
 
 
-for num in range(10): #we are repeating everything 10 times
+for num in range(1): #we are repeating everything 10 times
     #get initial start spot
     step_n_list = list()
-    x = np.random.uniform(0.5,1, 2) #starting point is outside our circle
-    v = np.random.uniform(0,1, 2)
+    x = np.random.uniform(0.5,20, 2) #starting point is outside our circle
+    v = np.random.uniform(0,vmax, 2)
     if np.linalg.norm(v) > vmax:
         v = v/np.linalg.norm(v) * vmax
     x0 = x
@@ -54,7 +54,7 @@ for num in range(10): #we are repeating everything 10 times
         
 
         #setting some parameters
-        epsilon = 0.01
+        epsilon =  0.01
         n_steps = 10000
         dt = 0.1
         
@@ -63,8 +63,8 @@ for num in range(10): #we are repeating everything 10 times
         for i in range(1, n_steps):
             #update v
             v = v + a * dt
-            if np.linalg.norm(v) > 1.0:
-                v = v/np.linalg.norm(v)
+            if np.linalg.norm(v) > vmax:
+                v = v/np.linalg.norm(v) * vmax
                 
             
             #take a step in space
@@ -80,16 +80,16 @@ for num in range(10): #we are repeating everything 10 times
             
             #get magnitude of a
             a = -a/np.linalg.norm(a) * amax#outside circle
-            if np.linalg.norm(x) < 1/2: #hit the breaks, inside circle
+            if np.linalg.norm(x) < (vmax**2/(2*amax)): #hit the breaks, inside circle
                 a = a/np.linalg.norm(a) * amax
             if to_plot:
                 plt.plot(x[0], x[1], '.')
             
                 
             #check if we are close enough to origin then stop
-            if np.linalg.norm(x) < epsilon:
-                print("DONE")
-                break
+            # if np.linalg.norm(x) < epsilon:
+            #     print("DONE")
+            #     break
         
         #record number of timesteps
         print(i)
@@ -114,6 +114,7 @@ for num in range(10): #we are repeating everything 10 times
             plt.title("Path with lambda = " +str(lam))
             
             plt.show()
+            
             plt.semilogy(x_list, label = "x")
             plt.plot(v_list, label = "v")
             plt.plot(a_list, label = "a")
